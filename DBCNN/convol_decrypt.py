26b602e906aa7c832d32fd9494616c9280a5bac1
kk612code <97612722+kk612code@users.noreply.github.com>
2022-01-12 23:21:12 +0800

    Add files via upload

---
+++ DBCNN/convol_decrypt.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import misc
+import sys
+import os
+import random
+
+def deconvolution(data, weight):
+    num = data.size
+    data = data.reshape((1, len(data)))
+    shape = weight.shape
+    wt_num = shape[1]
+    n_opt = wt_num - 1
+
+    ###disorder_1
+    tmp = data[0].copy()
+    t_n = (num // wt_num) * wt_num
+    for i in range(1, t_n):
+        j = (i % (num // wt_num)) * wt_num + (i // (num // wt_num))
+        data[0][j] = tmp[i]
+    for i in range(1, t_n):
+        data[0][t_n-i] = data[0][t_n-i] ^ data[0][t_n-i-1]
+
+    ###opt_1
+    for i in range(0, num):
+        for j in range(0, n_opt):
+            if (i-n_opt+j) < 0:
+                data[0][i] = int(data[0][i]) - int(weight[0][j])
+            else:
+                data[0][i] = int(data[0][i]) - int(weight[0][j] ^ data[0][i-n_opt+j])
+        data[0][i] = data[0][i] ^ weight[0][n_opt]
+
+    ###opt_2
+    #for i in range(0, num):
+    #    for j in range(0, n_opt):
+    #        if (n_opt - j) % 2 == 0:
+    #            if (i-n_opt+j) < 0:
+    #                data[0][i] = int(data[0][i]) + int(weight[0][j])
+    #            else:
+    #                data[0][i] = int(data[0][i]) + int(weight[0][j] ^ data[0][i-n_opt+j])
+    #        else:
+    #            if (i-n_opt+j) < 0:
+    #                data[0][i] = int(data[0][i]) - int(weight[0][j])
+    #            else:
+    #                data[0][i] = int(data[0][i]) - int(weight[0][j] ^ data[0][i-n_opt+j])
+    #    data[0][i] = data[0][i] ^ weight[0][n_opt]
+
+    return data[0]
+
+def layerNoWeight(weight, data):
+    data = deconvolution(data, weight)
+    return data
+
+
+def readImages(npy_path): #direct = './train/'
+    data = np.load(npy_path)
+    return data
+
+def allImageToLayer(data, wt): #0(r), 1(g), 2(b), 3(grey)
+    I0 = data
+    I0 = I0.reshape(I0.size)  # 二维图像转变成一维
+    xordt0 = layerNoWeight(wt, I0)  # 连接层运算
+    xordt0 = xordt0.reshape(data.shape[0], data.shape[1])
+    data = xordt0
+    return data
+
+def execute(data, n_layer, wt):
+    ###test
+    data = allImageToLayer(data, wt)
+
+    print('layer: %2d/%2d' % (n_layer, num_layers))
+    return data
+    ###test
+
+
+path = './data_stack/3x10/encrypt0.npy'
+data = readImages(path)
+weights = np.load('./data_stack/3x10/weights.npy')
+num_layers = len(weights)
+for i in range(0, num_layers):
+    data = execute(data, i+1, weights[num_layers-1-i])
+np.save('./data_stack/3x10/decrypt0.npy', data)
+plt.imshow(data, cmap='gray')
+plt.axis('off')
+plt.show()
+
+
+
+
+
---
+++ DBCNN/convol_encrypt.py
@@ -0,0 +1,127 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import time
+import random
+
+
+def convolution_weight(n1, n2):
+    mt = np.random.randint(0, 255, size=(n1, n2))
+    return np.array(mt, dtype=np.uint8)
+
+
+def convolution(data, weight):
+    shape = weight.shape
+    wt_num = shape[1]
+    data = data.reshape((1, len(data)))
+    dt_num = data.size
+
+    for i in range(0, wt_num-1):
+        tmp = np.insert(data[i], 0, 0)
+        tmp = np.delete(tmp, -1)
+        data = np.insert(data, i + 1, tmp, axis=0)
+
+    for i in range(0, wt_num):
+        data[i] = weight[0][wt_num-1-i] ^ data[i]
+
+    ###opt_1
+    for i in range(1, wt_num):
+            data[0] = np.add(data[0], data[i])
+
+    ###opt_2
+    # for i in range(1, wt_num):
+    #     if i % 2 == 1:
+    #         data[0] = np.add(data[0], data[i])
+    #     else:
+    #         data[0] = np.subtract(data[0], data[i])
+
+    ###disorder_1
+    tmp = data[0].copy()
+    t_n = (dt_num // wt_num) * wt_num
+    for i in range(1, t_n):
+        j = (i % wt_num) * (dt_num // wt_num) + (i // wt_num)
+        tmp[i] = tmp[i] ^ tmp[i-1]
+        data[0][j] = tmp[i]
+
+    ###disorder_2
+    # for i in range(1, dt_num):
+    #     data[0][i] = data[0][i] ^ data[0][i-1]
+
+    return data[0]
+
+
+def layerNoWeight(weight, data):
+    data = convolution(data, weight)
+    return data
+
+
+def readImages(direct): #direct = './train/'
+    data = []
+    for root, dirs, file in os.walk(direct):
+        for fl in file:
+            if os.path.splitext(fl)[1] == '.png':
+                # image = np.load(direct+fl)
+                image = plt.imread(direct+fl)
+                image = np.array(image[:, :, 0] * 255, np.uint8)
+                data.append(image)
+    return data
+
+#统计每个值出现的频率
+def distribute(data, maxv=256):
+    leng = len(data)
+    uni, count = np.unique(data, return_counts=True)
+    return np.pad(count, (0, 256 - len(count)), mode='constant')/leng #(1,256)
+
+#计算均匀分布（uniform）于给定数据（data）分布间的KL散度值
+uniform = [1./256. for i in range(0, 256)]
+def computUniform(data, pre=uniform):
+    data = data + np.spacing(1)
+    div = np.dot(pre, np.log(pre / data)) #点对点运算后再内积，pre是（1，256），data是（1，256）
+    return div
+
+def allImageToLayer(data, n1=1, n2=2): #0(r), 1(g), 2(b), 3(grey)
+    wt = convolution_weight(n1, n2) #获得初始化权值
+    num = len(data)
+    xordata = []
+    div0 = 0
+    for i in range(0, num):
+        I0 = data[i] #取单张图片data[i]
+        I0 = I0.reshape(I0.size)
+        xordt0 = layerNoWeight(wt, I0) #连接层运算
+        dis0 = distribute(xordt0)
+        div0 += computUniform(dis0)
+        xordt0 = xordt0.reshape(data[i].shape[0], data[i].shape[1])
+        xordata.append(xordt0)
+    return wt, xordata, div0
+
+def execute(data, n_layer):
+    ###test
+    wt, data, div0 = allImageToLayer(data, n1=1, n2=5)
+
+    print('layer %d/%d div %f' % (n_layer, num_layers, div0))
+    return wt, data
+    ###test
+
+
+data = readImages('./database/')
+weights = []
+num_layers = 4
+for i in range(0, num_layers):
+    wt, data = execute(data, i+1)
+    weights.append(wt)
+np.save('./data_stack/3x10/weights.npy', weights)
+plt.imshow(data[0], cmap='gray')
+plt.show()
+
+for i in range(0, len(data)):
+    np.save('./data_stack/3x10/encrypt'+str(i)+'.npy', data[i])
+plt.imsave('./data_stack/3x10/encrypt0.png', data[0], cmap='gray');
+
+
+
+
+
+
+
+
